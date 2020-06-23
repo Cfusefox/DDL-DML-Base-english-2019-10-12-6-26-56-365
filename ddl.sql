55106c29433bbae44bf6e58f1e553dc024695043
fox <1036674617@qq.com>
2020-06-23 21:52:10 +0800

    first finish

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,32 @@
 -- **Database level:**
 -- Display all the database
-
+show databases;
 -- Enter a certain database
-
+use database_name;
 -- Create a database
-
+create database database_name;
 -- Create the database of the designated character set
-
+create database database_name character set character_name;
 -- Display the creation information fo the database
-
+show create database database_name;
 -- Revise the codes of the database
-
+alter database database_name character set character_name;
 -- Delete a database
-
+drop database database_name;
 -- **Table level**
 -- Revise table name
-
+alter table table_old_name rename table_new_name;
 -- Revise the field's data type
-
+alter table table_name modify field data_type;
 -- Revise field name
-
+alter table table_name change field_old_name field_new_name;
 -- Add field
-
+alter table table_name add field_name data_type;
 -- Delete field
-
+alter table table_name drop field_name;
 -- Revise the table's storage engine
-
+alter table table_name engine = engine_name;
 -- Delete the table's foreign key restriant
-
+alter table table_name drop foreign key foreign_key_name;
 -- Delete a table
+drop table table_name;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,13 @@
 -- Insert record
-
+-- insert into table_name (field1, field2, ....) values (value1, value2, ...);
+insert into student (name, age, sex) VALUES ('test', 18, 'male');
 -- Revise record
-
+-- update table_name set field=value [where condition];
+UPDATE student set age=22 where name='test';
 --  Delete record
-
+-- delete from table_name [WHERE CONDITION];
+DELETE from student where name='test';
 -- Search record
+-- SELECT */fields FROM table_name [WHERE CONDITION];
+select * from student;
+select name, age, sex from student where sex = 'male';
